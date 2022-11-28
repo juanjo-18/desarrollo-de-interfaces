eec4c440548d0a2598253a5e367a435f0ef91911
juanjo-18 <juanjomedinadiaz@gmail.com>
2022-11-28 12:54:15 +0100

    liros

---
+++ prLibros2/src/application/IndexController.java
@@ -0,0 +1,86 @@
+package application;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+public class IndexController {
+	
+	@FXML
+	private TextField txtTitulo;
+	
+	@FXML
+	private ChoiceBox cbEditorial;
+	
+	@FXML
+	private TextField txtAutor;
+	
+	@FXML
+	private TextField txtPaginas;
+	
+	@FXML
+	private TableView <Libro> tableLibros;
+	
+	@FXML
+	private TableColumn <Libro, String> columnTitulo;
+	
+	@FXML
+	private TableColumn <Libro, String> columnEditorial;
+	
+	@FXML
+	private TableColumn <Libro, String> columnAutor;
+	
+	@FXML
+	private TableColumn <Libro, Integer> columnPaginas;
+	
+	@FXML
+	private Button btnAnadir;
+	
+	private ObservableList<Libro> listaLibros =
+		FXCollections.observableArrayList(
+				new Libro("La Biblia", "Planeta", "Jesús", 500)
+		);
+	
+	public ObservableList<String> listaEditoriales = 
+		FXCollections.observableArrayList(
+			"Planeta",
+			"Altaya",
+			"Kadokawa",
+			"Penguin Libros" 
+		);
+	
+	@FXML
+	private void initialize() {
+		
+		cbEditorial.setItems(listaEditoriales);
+		
+		columnTitulo.setCellValueFactory(new PropertyValueFactory<>("titulo"));
+		columnEditorial.setCellValueFactory(new PropertyValueFactory<>("editorial"));
+		columnAutor.setCellValueFactory(new PropertyValueFactory<>("autor"));
+		columnPaginas.setCellValueFactory(new PropertyValueFactory<>("paginas"));
+		
+		tableLibros.setItems(listaLibros); 
+	}
+	
+	public void anadirLibro(ActionEvent event) {
+		Libro l=new Libro(
+				txtTitulo.getText(),
+				cbEditorial.getValue().toString(),
+				txtAutor.getText(),
+				Integer.parseInt(txtPaginas.getText())
+				);
+		
+		listaLibros.add(l);
+	}
+	
+	@FXML
+	public void borrarLibro(ActionEvent event) {
+		System.out.println("Borrando libro");
+	}
+}
