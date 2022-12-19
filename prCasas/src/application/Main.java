e5c300aeac46492120baa90bb8cb448031982879
juanjo-18 <juanjomedinadiaz@gmail.com>
2022-12-19 12:28:41 +0100

    Casas

--- prCasas/src/application/Main.java
+++ prCasas/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -14,6 +15,8 @@ public class Main extends Application {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Index.fxml"));
 			Scene scene = new Scene(root,800,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.getIcons().add(new Image("/application/casa.png"));
+			primaryStage.setTitle("Nuestras casas");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
